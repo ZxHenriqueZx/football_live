65b8269e40e27e48d8adc5ab16bd7538e9aa805f
ZxHenriqueZx <henriquetargaryen123@gmail.com>
2023-11-25 18:21:28 -0300

    adicionei a função input_filter para filtrar o input

--- app/utils.py
+++ app/utils.py
@@ -4,3 +4,11 @@ def is_int(value):
         return True
     except ValueError:
         return False
+
+def input_filter(value, _type):
+    try:
+        _input = _type(input(value))
+        return _input
+    except ValueError:
+        print(f'Este valor não é do tipo: {_type}')
+

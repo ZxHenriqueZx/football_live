add296295ce70b99f2886220a9198cf6707fa18e
ZxHenriqueZx <henriquetargaryen123@gmail.com>
2023-11-25 13:57:24 -0300

    adicionei o método get para teste

--- app/api_football.py
+++ app/api_football.py
@@ -1,13 +1,35 @@
 import requests
 import dotenv
-import os
+from os import environ as env
 
 dotenv.load_dotenv()
 
+class Api:
+    def __init__(self, headers):
+        self.url = env['URL_API']
+        self.endpoints = {
+            "times": "teams",
+        }
+
+        self._headers = headers
+
+    def get(self, endpoint, params):
+        with requests.get(
+            self.url + self.endpoints[endpoint],
+            headers=self._headers,
+            params=params
+        ) as re:
+
+            self._data = re.json()
+            print(self._data)
+
 if __name__ == '__main__':
     headers = {
-        "X-RapidAPI-Key": os.environ['API_KEY'],
-        "X-RapidAPI-Host":os.environ['API_HOST'],
+        "X-RapidAPI-Key": env['API_KEY'],
+        "X-RapidAPI-Host": env['API_HOST'],
     }
-    with requests.get(os.environ['URL_API'] + 'timezone', headers=headers) as re:
-        print(re.json())
+
+    app = Api(headers)
+
+    app.get("times", {"id":121})
+

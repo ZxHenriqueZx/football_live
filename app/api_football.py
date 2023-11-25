3dab95319b1c0362909e2763ba557b35cdd4e467
ZxHenriqueZx <henriquetargaryen123@gmail.com>
2023-11-25 18:22:27 -0300

    implementei a função team_fav, para adicionar o time favorito do usuário

--- app/api_football.py
+++ app/api_football.py
@@ -2,6 +2,8 @@ import requests
 import dotenv
 from os import environ as env
 
+from utils import is_int, input_filter
+
 dotenv.load_dotenv()
 
 class Api:
@@ -12,16 +14,34 @@ class Api:
         }
 
         self._headers = headers
+        self.team = None
 
     def get(self, endpoint, params):
         with requests.get(
             self.url + self.endpoints[endpoint],
             headers=self._headers,
-            params=params
-        ) as re:
+            params=params) as re:
 
-            self._data = re.json()
-            print(self._data)
+            self.data = re.json()['response']
+            #print(self.data)
+
+    def team_fav(self, team_name):
+        self.get("times", {'search': team_name})
+
+        for i, item in enumerate(self.data):
+            team_name = item['team']['name']
+            team_country = item['team']['country']
+            
+            print(f'{i} - {team_name} - {team_country}')
+
+        while True:
+            index_team = input_filter('Escolha o time: ', int)
+
+            if index_team in range(len(self.data)):
+                self.team = self.data[index_team]
+                break
+            else:
+                print('Index Errado, escolha umas das opções acima')
 
 if __name__ == '__main__':
     headers = {
@@ -31,5 +51,6 @@ if __name__ == '__main__':
 
     app = Api(headers)
 
-    app.get("times", {"id":121})
+    app.team_fav("fla")
 
+    print(app.team)

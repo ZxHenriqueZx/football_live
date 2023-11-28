62ddddb044fbb650f68296e4662abc6935b0f1b9
ZxHenriqueZx <henriquetargaryen123@gmail.com>
2023-11-28 13:03:09 -0300

    modified:   main.py

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import dotenv
 from os import environ as env
 from app.api_football import Api
-from app.utils import is_int, input_filter
 
 if __name__ == "__main__":
     headers = {
@@ -11,29 +9,4 @@ if __name__ == "__main__":
 
     football_live = Api(headers)
 
-    options = [
-        'Definir Time preferido',
-        'Sair',
-    ]
-
-    def menu():
-        print()
-        for i, op in enumerate(options):
-            print(i, op)
-
-        index_option = input_filter('Escolha uma opção: ', int)
-
-        return index_option
-
-    print('=== FOOTBALL LIVE ===')
-    while True:
-        print()
-        option = menu()
-
-        if options[option] == 'Definir Time preferido':
-            football_live.team_fav(input_filter('Digite o nome do Time: ', str))
-            continue 
-
-        if options[option] == 'Sair':
-            break
 
